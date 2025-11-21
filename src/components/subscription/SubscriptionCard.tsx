89c1569a99c09f807e2084a4308ba4958f201f17
Daniel Azevedo <odanielfotografo@icloud.com>
2025-11-21 14:09:05 -0300

    refactor(build): Padroniza a pasta de componentes para 'ui' e corrige todas as importações

--- src/components/subscription/SubscriptionCard.tsx
+++ src/components/subscription/SubscriptionCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Check } from 'lucide-react'
-import { Button } from '../ui/Button'
+import { Button } from '../ui/button'
 import { StripeProduct } from '../../stripe-config'
 
 interface SubscriptionCardProps {
